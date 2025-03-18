a24241a2f857aad885ea9ac91206778e8a3e9fb1
yosr2003 <yosr.amamou@yahoo.com>
2025-03-18 09:39:31 +0100

    first commit

---
+++ projetchatbot/src/main/java/com/example/projetCHATBOT/projetchatbot/ProjetchatbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetCHATBOT.projetchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetchatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetchatbotApplication.class, args);
+	}
+
+}
